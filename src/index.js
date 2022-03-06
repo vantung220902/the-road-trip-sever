697874c19eeb1f0e47151f8ab338cf951828b7af
vantung220902 <84801062+vantung220902@users.noreply.github.com>
2022-03-06 16:39:43 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 43760;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const morgan = require('morgan');
 const router = require('./routes');
