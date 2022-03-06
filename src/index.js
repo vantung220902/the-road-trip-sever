282f8ebae5e8854b41c4c12e64dd5eb19bf84edf
vantung220902 <84801062+vantung220902@users.noreply.github.com>
2022-03-06 19:00:32 +0700

    update

--- src/index.js
+++ src/index.js
@@ -20,11 +20,18 @@ const io = new Server(server, {
 const passport = require('passport');
 app.use(passport.initialize());
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+});
 
 app.use(express.json());
 
